3b86232ca80af2d942d0b946dabc1329382192be
hysryt <hysryt@gmail.com>
2020-03-10 22:03:45 +0900

    feat: デコード処理追加

--- src/base64.js
+++ src/base64.js
@@ -1,4 +1,5 @@
 const base64 = {}
+const map = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+.';
 
 base64.encode = function(str) {
   const chars = str.split('').map(c => {
@@ -15,7 +16,6 @@ base64.encode = function(str) {
     encodedCharBinaries.push(binary.substr(idx, 6));
     idx += 6;
   }
-  const map = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+.';
   const encodedChars = encodedCharBinaries.map(b => {
     const code = parseInt(b, 2);
     return map.charAt(code);
@@ -26,7 +26,17 @@ base64.encode = function(str) {
 }
 
 base64.decode = function(str) {
-  return str;
+  let decoded = str.split('').filter(c => c !== '=').map(c => {
+    return ('000000' + map.indexOf(c).toString(2)).slice(-6);
+  }).join('');
+  decoded = decoded.slice(0, decoded.length - decoded.length % 8);
+  if (decoded.length !== 0) {
+    decoded = new Array(decoded.length/8).fill(0).map((_,i) => {
+      const code = parseInt(decoded.substr(i * 8, 8), 2);
+      return String.fromCharCode(code);
+    }).join('');
+  }
+  return decoded;
 }
 
 exports.base64 = base64;
